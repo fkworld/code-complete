1dc0768ee2ef98bb7999db461c9ea2d4044a3efa
fkworld <fkworld@foxmail.com>
2026-01-27 12:07:05 +0000

    fix(build-time-utils): 移动 vitest 相关依赖到根路径下

--- packages/build-time-utils/src/get-vitest-config.ts
+++ packages/build-time-utils/src/get-vitest-config.ts
@@ -1,11 +1,13 @@
+// TODO 注意目前 vitest 不支持动态解析 v8 的来源，所以 @vitest/coverage-v8 还是需要安装在根目录下
+// TODO jsdom 同理
+// TODO issue https://github.com/vitest-dev/vitest/issues/9304
+
 import type { InlineConfig } from "vitest/node";
 
 export function getVitestConfig(): InlineConfig {
   return {
     environment: "jsdom",
     coverage: {
-      // TODO 注意目前 vitest 不支持动态解析 v8 的来源，所以 @vitest/coverage-v8 还是需要安装在根目录下
-      // TODO issue https://github.com/vitest-dev/vitest/issues/9304
       provider: "v8",
       reporter: ["text", "text-summary"],
     },
