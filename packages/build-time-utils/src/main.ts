22f48b482f115c8707465c4babe37b34c46cc418
fkworld <fkworld@foxmail.com>
2026-02-07 07:00:19 +0000

    fix(build-time-utils): 将 getHtmlInjectBuildResult 收至 getViteConfig

--- packages/build-time-utils/src/main.ts
+++ packages/build-time-utils/src/main.ts
@@ -2,6 +2,5 @@ export { eslintIt } from "./gen-utils/eslint-it";
 export { getGenInfos } from "./gen-utils/get-gen-infos";
 export { logGenOutput } from "./gen-utils/log-gen-output";
 export { prettierIt } from "./gen-utils/prettier-it";
-export { getHtmlInjectBuildResult } from "./get-html-inject-build-result";
-export { getViteConfig } from "./get-vite-config";
 export { getVitestConfig } from "./get-vitest-config";
+export { getViteConfig } from "./vite/get-vite-config";
--- packages/build-time-utils/src/vite/get-html-inject-build-result.ts
+++ packages/build-time-utils/src/vite/get-html-inject-build-result.ts
@@ -2,8 +2,8 @@ import path from "node:path";
 
 import { build } from "vite";
 
-export async function getHtmlInjectBuildResult(options: { injectFilename: string }): Promise<string> {
-  const { injectFilename } = options;
+export async function getHtmlInjectBuildResult(options: { filepath: string }): Promise<string> {
+  const { filepath } = options;
 
   const result = await build({
     // ! 需要指定 vite root，避免与主项目 vite 配置冲突
@@ -14,7 +14,7 @@ export async function getHtmlInjectBuildResult(options: { injectFilename: string
       // 不压缩
       minify: false,
       lib: {
-        entry: injectFilename,
+        entry: filepath,
         formats: ["iife"],
         name: "inject",
       },
