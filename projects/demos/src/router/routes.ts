ba282b9380024f073bd21e71b38e95791b50f1c4
fkworld <fkworld@foxmail.com>
2026-02-07 09:55:20 +0000

    refactor(demos): 更严格的路由配置

--- projects/demos/src/router/routes.ts
+++ projects/demos/src/router/routes.ts
@@ -1,16 +1,19 @@
-import type { RouteRecordRaw } from "vue-router";
+import type { RouteRecordSingleView } from "vue-router";
 
-export const ROUTES: Array<RouteRecordRaw> = [
-  {
-    path: "/home",
-    component: () => import("@/views/home/index"),
-  },
-  {
-    path: "/debug/debug-mqtt",
-    component: () => import("@/views/debug/debug-mqtt/index"),
-  },
-  {
-    path: "/debug/debug-api",
+/* eslint perfectionist/sort-objects: "error" */
+/** 更严格的路由配置
+ * - 扁平化
+ * - 按 path 排序（通过 eslint 实现）
+ * - 只允许写 component 和 meta 字段
+ */
+export const ROUTES: Record<string, Pick<RouteRecordSingleView, "component" | "meta">> = {
+  "/debug/debug-api": {
     component: () => import("@/views/debug/debug-api/index"),
   },
-];
+  "/debug/debug-mqtt": {
+    component: () => import("@/views/debug/debug-mqtt/index"),
+  },
+  "/home": {
+    component: () => import("@/views/home/index"),
+  },
+};
