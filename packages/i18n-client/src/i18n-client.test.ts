d0ffe3fae7779b6a44a1c9347daf66748a622b8b
fkworld <fkworld@foxmail.com>
2026-03-04 11:14:32 +0000

    feat(i18n-client): 简化测试代码

--- packages/i18n-client/src/i18n-client.test.ts
+++ packages/i18n-client/src/i18n-client.test.ts
@@ -1,8 +1,8 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { createI18nClient } from "./i18n-client";
 
-const MESSAGES = {
+const I18N_MESSAGES = {
   没有配置任何语言: {},
   配置中文但内容为空: { zh: "" },
   仅配置中文: { zh: "中文" },
@@ -12,51 +12,32 @@ const MESSAGES = {
 } satisfies Record<string, { zh?: string; en?: string }>;
 
 describe(createI18nClient.name, () => {
-  const { t } = createI18nClient<keyof typeof MESSAGES>({
-    messages: MESSAGES,
-    language: "zh",
-    isDebugMode: false,
-  });
+  it("normal mode", () => {
+    const { t } = createI18nClient<keyof typeof I18N_MESSAGES>({
+      messages: I18N_MESSAGES,
+      language: "zh",
+      isDebugMode: false,
+    });
 
-  beforeEach(() => {
-    localStorage.clear();
-  });
-
-  it("should return zh message", () => {
     expect(t("仅配置中文")).toBe("中文");
     expect(t("都配置")).toBe("中文");
-  });
-
-  it("should return key message", () => {
     expect(t("没有配置任何语言")).toBe("没有配置任何语言");
     expect(t("配置中文但内容为空")).toBe("配置中文但内容为空");
     expect(t("仅配置英文")).toBe("仅配置英文");
-  });
-
-  it("should return zh message with params", () => {
     expect(t("带参数")).toBe("中文 {hello}");
     expect(t("带参数", { hello: "你好" })).toBe("中文 你好");
     expect(t("带参数", { wrongKey: "wrongKey" })).toBe("中文 hello");
-  });
-
-  it("should return key message when pass wrong key", () => {
     // @ts-expect-error 测试传入不存在的 key
     expect(t("wrong key")).toBe("wrong key");
   });
-});
 
-describe(`${createI18nClient.name}debugMode`, () => {
-  const { t } = createI18nClient<keyof typeof MESSAGES>({
-    messages: MESSAGES,
-    language: "zh",
-    isDebugMode: true,
-  });
+  it("debug mode", () => {
+    const { t } = createI18nClient<keyof typeof I18N_MESSAGES>({
+      messages: I18N_MESSAGES,
+      language: "zh",
+      isDebugMode: true,
+    });
 
-  beforeEach(() => {
-    localStorage.clear();
-  });
-
-  it("should return zh message and en message", () => {
     expect(t("没有配置任何语言")).toBe("没有配置任何语言 / 没有配置任何语言");
     expect(t("配置中文但内容为空")).toBe("配置中文但内容为空 / 配置中文但内容为空");
     expect(t("仅配置中文")).toBe("中文 / 仅配置中文");
